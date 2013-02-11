e9d7dfe2c378d73c27fc1071cfeeb5f96ec5f78f
Scott Stadt <sstadt@fullsail.edu>
2013-02-11 16:37:43 -0500

    Slider Set Up
    Initial reorganization of the DOM in preparation for scrolling
    completed. The second half of the scroller items is placed at the
    beginning of the list and the top position of all scroller elements is
    adjusted to make the first item visible. Work on the next and previous
    buttons started.

--- jquery.VerticalSlider.js
+++ jquery.VerticalSlider.js
@@ -7,9 +7,57 @@
 			var that = $(this),
 				tempNext = '#' + that.attr('id') + '-next',
 				tempPrev = '#' + that.attr('id') + '-prev',
+				tempContent = '#' + that.attr('id') + '-content',
 				next = $(tempNext),
-				prev = $(tempPrev)
+				prev = $(tempPrev),
+				content = $(tempContent),
+				count = 0
 			;
+			
+			that.children().each(function(){count++;});
+			var half = Math.floor(count/2);
+			
+			// crop off the second half, one by one, and stick them at the beginning
+			for (var i=0; i<half; i++) {
+				tempItem = that.children(':last').detach();
+				that.prepend(tempItem);
+			}
+			
+			console.log(that.children().eq(half));
+			
+			// then, move the top to the original first's position
+			that.css({"position":"relative"});
+			that.children().css({
+				"position":"absolute",
+				"top": -=(content.children().eq(half).position().top)
+			});
+			
+			console.log('count: ' + count);
+			console.log('half: ' + half);
+			
+			// -= animate the slider
+			next.click(function(){
+				var lastPos = that.children(':last').css("top");
+			
+				// push the slider down
+				that.children().animate({"top":+=(that.children().height())}, 600);
+				
+				// move the first slider element to where the last one was
+				that.children(':first').css({"top":lsstPos});
+				
+				return false;
+			});
+			
+			// += animate the slider
+			prev.click(function(){
+				// push the slider up
+				
+				// move the last slider element to where the first one was
+				
+				return false;
+			});
+			
+			//$(".contentbox-wrapper").animate({"left": -($(id).position().left)}, 600);
 			
 			console.log(that);
 			console.log(next);
