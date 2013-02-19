811e7ad943bcc320cc2110ddf3f30bd12aebd009
Scott Stadt <sstadt@fullsail.edu>
2013-02-19 13:11:25 -0500

    Polish Commit
    Began restyling of the example page in preparation for html
    instructions. Disabled buttons during transitions and changed scroll
    down animation to make the scroller more seamless.

--- jquery.VerticalSlider.js
+++ jquery.VerticalSlider.js
@@ -7,7 +7,11 @@
 			var that = $(this),
 				next = $('#' + that.attr('id') + '-next'),
 				prev = $('#' + that.attr('id') + '-prev'),
-				content = $('#' + that.attr('id') + '-content')
+				content = $('#' + that.attr('id') + '-content'),
+				active = true;
+				vsToggle = function(){
+					active = !active;
+				}
 			;
 			
 			// crop off the last item, then put it at the beginning
@@ -22,20 +26,32 @@
 			
 			next.click(function(){
 			
-				// animate the top element shrinking, pull it off the top, and stick at the end
-				content.children(':first').hide(600, function(){
-					$(this).detach().appendTo(content).show(0);
-				});
+				if (active) {
+				
+					vsToggle();
+			
+					// animate the top element shrinking, pull it off the top, and stick at the end
+					content.children(':first').hide(600, function(){
+						$(this).detach().appendTo(content).show(0,vsToggle);
+					});
+				
+				}
 				
 				return false;
 			});
 			
 			prev.click(function(){
 			
-				// hide, then remove the bottom element, then stick it at the top and show
-				content.children(':last').hide(0,function(){
-					$(this).detach().prependTo(content).show(600);
-				});	
+				if (active) {
+				
+					vsToggle();
+			
+					// hide, then remove the bottom element, then stick it at the top and show
+					content.children(':last').hide(0,function(){
+						$(this).detach().prependTo(content).slideDown(600,vsToggle);
+					});
+				
+				}
 				
 				return false;
 			});
